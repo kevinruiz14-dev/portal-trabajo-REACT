f04af807fae7413c9118306b9376f295dd549230
SaritaDev97 <saritasantos261@gmail.com>
2026-06-01 08:45:51 -0600

    Arreglo de error en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,40 +3,12 @@ import Navbar from './layouts/Navbar';
 import Footer from './layouts/Footer';
 import Dashboardlayout from './layouts/Dashboardlayout';
 import Home from './pages/Home';
-<<<<<<< HEAD
-import DetalleEmpleo from './pages/empleo/DetalleEmpleo';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/*" element={
-          <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-            <Navbar />
-            <main style={{ flex: 1, padding: '2rem', maxWidth: '1200px', margin: '0 auto', width: '100%' }}>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/empleo/:id" element={<DetalleEmpleo />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        } />
-        <Route path="/dashboard/*" element={
-          <Dashboardlayout>
-            <Routes>
-              <Route path="/" element={<h2>Bienvenido al Panel</h2>} />
-              <Route path="/perfil" element={<h2>Mi Perfil</h2>} />
-            </Routes>
-          </Dashboardlayout>
-        } />
-      </Routes>
-=======
 import Login from './pages/auth/Login';
 import SeleccionTipoRegistro from './pages/auth/SeleccionTipoRegistro';
 import RegisterUsuario from './pages/auth/RegisterUsuario';
 import RegisterEmpresa from "./pages/auth/RegisterEmpresa";
-  
+import DetalleEmpleo from './pages/empleo/DetalleEmpleo';
+
 function App() {
   return (
     <Router>
@@ -44,15 +16,15 @@ function App() {
       <main style={{ minHeight: '80vh', padding: '2rem', maxWidth: '1200px', margin: '0 auto', width: '100%' }}>
         <Routes>
           <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/registro" element={<SeleccionTipoRegistro />} />
-            <Route path="/register-usuario" element={<RegisterUsuario />} />
-            <Route path="/register-empresa" element={<RegisterEmpresa />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registro" element={<SeleccionTipoRegistro />} />
+          <Route path="/register-usuario" element={<RegisterUsuario />} />
+          <Route path="/register-empresa" element={<RegisterEmpresa />} />
         </Routes>
       </main>
       <Footer />
->>>>>>> 05b5cd2 (Registro usuario y empresa funcional desde React)
     </Router>
   );
 }
+
 export default App;
